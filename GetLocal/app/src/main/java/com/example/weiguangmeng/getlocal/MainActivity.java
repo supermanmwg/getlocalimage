d4a5ea41a79446d9e67f541a7ff821dad22d0f10
supermanmwg <554607289@qq.com>
2016-01-13 23:20:38 +0800

    get local file only

---
+++ GetLocal/app/src/main/java/com/example/weiguangmeng/getlocal/MainActivity.java
@@ -0,0 +1,91 @@
+package com.example.weiguangmeng.getlocal;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.ParcelFileDescriptor;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.FileDescriptor;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button getLocalButton;
+    private Button takePhotoButton;
+    private ImageView displayImage;
+    private final int PHOTO_PICKER_ID = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getLocalButton = (Button) findViewById(R.id.get_local_image);
+        takePhotoButton = (Button) findViewById(R.id.take_photo);
+        displayImage = (ImageView) findViewById(R.id.display_image);
+
+        getLocalButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+                startActivityForResult(Intent.createChooser(intent,
+                        "Complete action using"), PHOTO_PICKER_ID);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(PHOTO_PICKER_ID == requestCode) {
+            Uri imageUri = data.getData();
+            displayImage.setImageURI(imageUri);
+            Log.d("haha", "image uri is " + imageUri.toString());
+           /* try {
+                Bitmap bitmap = getBitmapFromUri(imageUri);
+                if(null != bitmap) {
+                 //   displayImage.setImageBitmap(bitmap);
+                }
+            } catch (IOException e) {
+            }
+*/
+        }
+    }
+
+    //UPDATED!
+    public String getPath(Uri uri) {
+        String[] projection = { MediaStore.Images.Media.DATA };
+        Cursor cursor = managedQuery(uri, projection, null, null, null);
+        if(cursor!=null)
+        {
+            //HERE YOU WILL GET A NULLPOINTER IF CURSOR IS NULL
+            //THIS CAN BE, IF YOU USED OI FILE MANAGER FOR PICKING THE MEDIA
+            int column_index = cursor
+                    .getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+        }
+        else return null;
+    }
+
+    private Bitmap getBitmapFromUri(Uri uri) throws IOException {
+        ParcelFileDescriptor parcelFileDescriptor =
+                getContentResolver().openFileDescriptor(uri, "r");
+        FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
+        Bitmap image = BitmapFactory.decodeFileDescriptor(fileDescriptor);
+        parcelFileDescriptor.close();
+        return image;
+    }
+
+}
